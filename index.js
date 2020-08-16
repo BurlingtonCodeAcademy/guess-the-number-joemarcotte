2a0e2c400c8d876ed16ee1d76e3b5664bdca61e1
joemarcotte <61711467+joemarcotte@users.noreply.github.com>
2020-08-16 15:27:57 -0400

    all functionality works

--- index.js
+++ index.js
@@ -1,79 +1,67 @@
-const readline = require("readline"); {
-  const rl = readline.createInterface(process.stdin, process.stdout);
+const readline = require("readline");
+{
+	const rl = readline.createInterface(process.stdin, process.stdout);
 
-  function ask(questionText) {
-    return new Promise((resolve, reject) => {
-      rl.question(questionText, resolve);
-    });
-  }
+	function ask(questionText) {
+		return new Promise((resolve, reject) => {
+			rl.question(questionText, resolve);
+		});
+	}
 
-  function randomNum(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-  start();
-  async function start() {
-    console.log(
-      "Let's play a game where you (Joe) make up a number and I (computer) try to guess it."
-    );
+	function randomNum(min, max) {
+		return Math.floor(Math.random() * (max - min + 1) + min);
+	}
+	start();
+	async function start() {
+		console.log(
+			"Let's play a game where you (Human) make up a number and I (DellPc) try to guess it."
+		);
 
-    let secretNum = await ask(
-      "What is your secret number?\nI won't peek, I promise...\n"
-    );
-    let min = 0; //min
-    let max = 100; //max
-    let contestantOne = randomNum(min, max); //computers guess
+		let secretNum = await ask(
+			"What is your secret number?\nI won't peek, I promise..If you try and cheat prepare to meet your DOOOOOOM!!!!!\n"
+		);
+		let min = 0; //min
+		let max = 100; //max
+		let dellGuess = randomNum(min, max); //computers guess
 
-    let answer = await ask(`Is the answer ${contestantOne}, "yes" or "no"?`);
-    // Now try and complete the program.
-    if (answer === "yes") {
-      console.log(`Congrats you won!`);
-      process.exit();
-    } else {
-      while (answer !== "yes") {
-        var highLow = await ask(`Is it higher or lower ?`);
+		let answer = await ask(`Is the answer ${dellGuess}, "yes" or "no"?`);
+		// Now try and complete the program.
+		if (answer === "yes") {
+			console.log(`Congrats you won!`);
+			process.exit();
+		} else {
+			while (answer !== "yes") {
+				var highLow = await ask(`Is it higher or lower ?`);
 
-        if (highLow === "higher") {
-          min = contestantOne + 1;
-          contestantOne = randomNum(min, max);
-        } else if (highLow === "lower") {
-          max = contestantOne - 1;
+				if (highLow === "higher") {
+					min = contestantOne + 1;
+					contestantOne = randomNum(min, max);
+				} else if (highLow === "lower") {
+					max = contestantOne - 1;
 
-          contestantOne = randomNum(min, max);
-        } else {
-          console.log("input not recognized");
-        } //catch all
-        answer = await ask(`Is the answer ${contestantOne}, "yes" or "no"?`);
-        if (answer === "yes") {
-          console.log("Victory is Yours!!! Would you like to play again? This time I DellPc will think of a number!");
+					contestantOne = randomNum(min, max);
+				} else {
+					console.log("input not recognized");
+				} //catch all
+				answer = await ask(`Is the answer ${contestantOne}, "yes" or "no"?`);
 
-        }
-
-      }
-
-    }
-  }
-} //   function contestantTwo(min, max);
-
-// function ask(questionText) {
-//  return new Promise((resolve, reject) => {
-//    rl.question(questionText, resolve);
-//  });
-//}
-
-
-//function randomNum(min, max) {
-//  return Math.floor(Math.random() * (max - min + 1) + min);
-//}
+//-----------------------Cheat Function---------------------------------//
+				if (max <= Math.ceil((min + max) / 2)) {
+					console.log("'YOU CHEATED FEED HIM TO THE GATORS!!!!" + "\n" + "GOODBYE FOREVER YOU FILTHY ANIMAL!'");
+					process.exit();
+				}
+		
+//----------------------Win and  Sign out------------------//				
+				if (answer === "yes") {
+					console.log(
+						"Victory is Yours!!! Would you like to play again? This time I DellPc will think of a number!"
+					);
+					process.exit();
+				}
+			}
+		}
+	}
+}
 
 
-//start()
-//async function start() {
 
-//  console.log("This time I Dell will pick a number and you the user will have to guess?");
-//  let randomNum = await ask(
-//    "What is your guess going to be? You better make it a good one!!!"
-//  );
-//  let contestantTwo = await ask(`Is the answer ${contestantTwo}, "yes" or "no"?`);
-//}
-//
-//}
